9f45f82de50dcc6f628745d6b5f78399b2fc1873
SuperCater <supercater500@gmail.com>
2024-01-14 14:47:13 -0500

    build fix

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,7 +2,6 @@ import { defineConfig } from "tsup";
 
 export default defineConfig({
   entry: ["src/index.ts"],
-  target: "node",
   format: ["esm"], // Build for commonJS and ESmodules
   dts: true, // Generate declaration file (.d.ts)
   splitting: false,
