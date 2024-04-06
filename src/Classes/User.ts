17cee164b80384f89e31bebc326a1a5e61dbb050
SuperCater <supercater500@gmail.com>
2024-04-06 16:47:14 -0400

    new methods

--- src/Classes/User.ts
+++ src/Classes/User.ts
@@ -23,3 +23,5 @@ class User {
 	
 	
 }
+
+export default User;
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,25 @@
+import User from "./Classes/User.js";
 import FetchHandler from "./Modules/fetchHandler.js";
+import { RawUserData } from "./Types/UserTypes.js";
 
 class WrapBlox {
-	
-	fetchUser = async (userId : number) => {
+	/**
+	 * Get the raw data of a user
+	 * @param userId The ID of the user to fetch
+	 * @returns The raw data of the user
+	 */
+	fetchRawUser = async (userId : number) : Promise<RawUserData> => {
 		return await FetchHandler.fetch('GET', 'Users', `/users/${userId}`);
+	}
+	
+	/**
+	 * Gets the user object of a user
+	 * @param userId The ID of the user to fetch
+	 * @returns The user object
+	 */
+	fetchUser = async (userId : number) => {
+		const rawData = await this.fetchRawUser(userId);
+		return new User(rawData);
 	}
 }
 
