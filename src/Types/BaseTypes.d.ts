4191fbef54b430e6d1d79a8964ad5d541c31ffc1
SuperCater <supercater500@gmail.com>
2024-04-09 22:47:26 -0400

    new types

--- src/Types/BaseTypes.d.ts
+++ src/Types/BaseTypes.d.ts
@@ -1,3 +1,5 @@
-export type ValidUrls = "Users" | "Groups" | "Thumbnails"
+export type ValidUrls = "Users" | "Groups" | "Thumbnails" | "Friends"
 
 export type HttpMethods = "GET" | "POST" | "PUT" | "DELETE"
+
+export type SortOrder = "Asc" | "Desc"
--- src/Types/UserTypes.d.ts
+++ src/Types/UserTypes.d.ts
@@ -8,3 +8,28 @@ export type RawUserData = {
 	created : string,
 	description : string,
 }
+
+export type RawFriendData = {
+	isOnline : boolean,
+	isDeleted : boolean,
+	friendFrequentScore : number,
+	friendFrequentRank : number,
+} & RawUserData;
+
+
+export type RawFriendRequest = {
+	friendRequest : {
+		sentAt : string,
+		senderId : number,
+		sourceUniverseId : number,
+		originSourceType : number,
+		contactName : string,
+	},
+	mutualFriendsList : string[],
+} & RawUserData
+
+export type BirthData = {
+	birthMonth : number,
+	birthDay : number,
+	birthYear : number,
+}
