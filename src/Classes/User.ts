e102bdc5d2f880868c548f3bb47de35e082f1cfb
SuperCater <supercater500@gmail.com>
2024-04-09 22:47:41 -0400

    extend user class

--- src/Classes/User.ts
+++ src/Classes/User.ts
@@ -1,4 +1,6 @@
-import { RawUserData } from "../Types/UserTypes.js";
+import { RawFriendData, RawUserData } from "../Types/UserTypes.js";
+import WrapBlox from "../index.js";
+import Friend from "./Friend.js";
 
 class User {
 	rawData : RawUserData;
@@ -6,17 +8,32 @@ class User {
 	name : string;
 	displayName : string;
 	description : string;
+	client : WrapBlox;
 	
 	
-	constructor(rawdata : RawUserData) {
+	constructor(client : WrapBlox, rawdata : RawUserData) {
 		this.rawData = rawdata;
 		this.id = rawdata.id;
 		this.name = rawdata.name;
 		this.displayName = rawdata.displayName;
 		this.description = rawdata.description;
+		this.client = client;
 	}
 	
+	async fetchFriends() : Promise<Friend[]> {
+		const ret = await this.client.fetchHandler.fetch('GET', 'Friends', `/users/${this.id}/friends`);
+		return ret.data.map((friend : RawFriendData) => {
+			return new Friend(this.client, friend);
+		});
+	}
 	
+	async fetchFriendCount() : Promise<number> {
+		return (await this.client.fetchHandler.fetch('GET', 'Friends', `/users/${this.id}/friends/count`)).count;
+	}
+	
+	async fetchNameHistory(): Promise<string[]> {
+		return (await this.client.fetchHandler.fetchAll('GET', 'Users', `/users/${this.id}/name-history`)).map((name : {name : string}) => name.name);
+	}
 }
 
 export default User;
