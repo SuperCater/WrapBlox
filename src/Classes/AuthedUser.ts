c51e63c82a480976d39ad336f6862d7fb9634a82
SuperCater <supercater500@gmail.com>
2024-04-09 22:47:56 -0400

    authed user

---
+++ src/Classes/AuthedUser.ts
@@ -0,0 +1,32 @@
+import { BirthData, RawFriendData, RawFriendRequest } from "../Types/UserTypes.js";
+import FriendRequest from "./FriendRequest.js";
+import User from "./User.js";
+
+class AuthedUser extends User {
+	
+	async fetchBirthdate()  {
+		return await this.client.fetchHandler.fetch('GET', 'Users', `/users/${this.id}/birthdate`);
+	}
+	
+	async setBirthdate(date : BirthData & {password : string}) : Promise<void> {
+		return await this.client.fetchHandler.fetch('POST', 'Users', `/users/${this.id}/birthdate`, undefined, date);
+	}
+	
+	async setDescription(description : string) : Promise<void> {
+		return await this.client.fetchHandler.fetch('POST', 'Users', `/users/${this.id}/description`, undefined, {description});
+	}
+	
+	async fetchCountryCode() : Promise<string> {
+		return (await this.client.fetchHandler.fetch('GET', 'Users', `/users/${this.id}/country`)).countryCode;
+	}
+	
+	async fetchFriendRequests() {
+		const ret = await this.client.fetchHandler.fetchAll('GET', 'Friends', "/my/friend-requests");
+		
+		return ret.map((friend : RawFriendRequest) => {
+			return new FriendRequest(this.client, friend);
+		});
+	}
+}
+
+export default AuthedUser;
