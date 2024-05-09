3670b2cab125119918234286b87701120c4b344a
SuperCater <supercater500@gmail.com>
2024-05-09 03:43:06 -0400

    friend format

--- src/Classes/Friend.ts
+++ src/Classes/Friend.ts
@@ -1,10 +1,13 @@
 import WrapBlox, { RawFriendData } from "../index.js";
 import User from "./User.js";
 
+
 class Friend extends User {
 	friendFrequentScore: number;
 	isDeleted: boolean;
 	isOnline: boolean;
+	
+	
 	constructor(client : WrapBlox, rawdata : RawFriendData) {
 		super(client, rawdata);
 		this.friendFrequentScore = rawdata.friendFrequentScore;
