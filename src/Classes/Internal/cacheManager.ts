9e39d9d88a5bcb30ae14caecddb9d99c8ef603c2
SuperCater <supercater500@gmail.com>
2024-05-09 03:43:11 -0400

    cachemanager

---
+++ src/Classes/Internal/cacheManager.ts
@@ -0,0 +1,51 @@
+class CacheManager<T, V> {
+	cache = new Map<T, {
+		value: V,
+		time: number,
+	}>();
+	
+	timeout = 1000 * 60 * 5; // How long the cache should last
+	
+	constructor(values : [T, V][] = []) {
+		for (const [key, value] of values) {
+			this.setValues(key, value);
+		}
+	}
+	
+	setTimeout = (time: number) => {
+		this.timeout = time;
+	}
+	
+	setValues = (key: T, value: V) => {
+		this.cache.set(key, {
+			value: value,
+			time: Date.now(),
+		});
+	}
+	
+	rawGetValues = (key: T) => {
+		const data = this.cache.get(key);
+		if (!data) return undefined;
+		if (Date.now() - data.time > this.timeout) {
+			this.cache.delete(key);
+			return undefined;
+		}
+		return data
+	}
+	
+	getValues = (key: T) => {
+		return this.rawGetValues(key)?.value;
+	}
+	
+	deleteValues = (key: T) => {
+		return this.cache.delete(key);
+	}
+	
+	clearCache = () => {
+		this.cache.clear();
+	}
+	
+	
+}
+
+export default CacheManager;
