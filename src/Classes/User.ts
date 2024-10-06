fb4924b2d4d1e10d806076350dc665ac5e7672ea
Purple_Creativity <thepurplecreativity@gmail.com>
2024-10-06 15:23:40 +0200

    Overhaul User.ts

--- src/Classes/User.ts
+++ src/Classes/User.ts
@@ -1,107 +1,101 @@
-import type { APIUserGroup, AvatarImageTypes, RawFriendData, RawUserData } from "../Types/UserTypes.js";
+import type { APIUserGroup, AvatarImageTypes, FriendMetadata, RawFriendData, RawUserData, UserAvatarV1, UserAvatarV2 } from "../Types/UserTypes.js";
 import type WrapBlox from "../index.js";
-import type { AwardedBadge, OwnedItem } from "../index.js";
+import type { AwardedBadge, OwnedItem, SortOrder } from "../index.js";
 // import type Friend from "./Friend.js";
 import UserRoleManager from "./UserRoleManager.js";
 
 import Factory from "./Internal/factory.js";
-import type Friend from "./Friend.js";
+import Friend from "./Friend.js";
 import { AvatarSize, ItemTypes } from "../Types/Enums.js";
+import factory from "./Internal/factory.js";
 
 
 
 class User {
+	client: WrapBlox;
 	rawData: RawUserData;
+
 	id: number;
 	name: string;
 	displayName: string;
 	description: string;
-	client: WrapBlox;
+	hasVerifiedBadge: boolean;
+	externalAppDisplayName?: string;
+	isBanned : boolean;
+	joinDate: Date;
 
+	accountAge: number;
 
-	constructor(client: WrapBlox, rawdata: RawUserData) {
-		this.rawData = rawdata;
-		this.id = rawdata.id;
-		this.name = rawdata.name;
-		this.displayName = rawdata.displayName;
-		this.description = rawdata.description;
+	constructor(client: WrapBlox, rawData: RawUserData) {
 		this.client = client;
-		
-		
+		this.rawData = rawData;
+
+		this.id = rawData.id;
+		this.name = rawData.name;
+		this.displayName = rawData.displayName;
+		this.description = rawData.description;
+		this.hasVerifiedBadge = rawData.hasVerifiedBadge;
+		this.externalAppDisplayName = rawData.externalAppDisplayName;
+		this.isBanned = rawData.isBanned;
+		this.joinDate = new Date(rawData.created);
+
+		this.accountAge = Math.ceil(Math.abs(new Date().getTime() - new Date(rawData.created).getTime()) / (1000 * 60 * 60 * 24));
 	}
 
-	async fetchFriends(): Promise<Friend[]> {
-		const ret = await this.client.fetchHandler.fetch('GET', 'Friends', `/users/${this.id}/friends`);
+	/*
+		Methods related to the UsersV1 API
+		Docs: https://users.roblox.com/docs/index.html
+	*/
 
-		return ret.data.map((friend: RawFriendData) => {
-			Factory.createFriend(this.client, friend, this);
-		});
-	}
+	async fetchUsernameHistory(maxResults?: number, useCache = true): Promise<string[]> {
+		const returnData = [] as string[];
+		let nextPageCursor = "NONE";
 
-	async fetchFriendCount(): Promise<number> {
-		return (await this.client.fetchHandler.fetch('GET', 'Friends', `/users/${this.id}/friends/count`)).count;
-	}
+		loop: while (nextPageCursor) {
+			const request = await this.client.fetchHandler.fetch("GET", "Users", `/users/${this.id}/username-history`, {
+				usecache: useCache,
+				params: {
+					limit: 100,
+					cursor: nextPageCursor !== "NONE" && nextPageCursor || undefined,
+				}
+			})
 
-	async fetchNameHistory(): Promise<string[]> {
-		return (await this.client.fetchHandler.fetchAll('GET', 'Users', `/users/${this.id}/name-history`)).map((name: { name: string }) => name.name);
-	}
+			for (const entry of request.data) {
+				returnData.push(entry.name);
 
-
-	async fetchRawRoles(includelocked = false, includeNotificationPreferences = false): Promise<APIUserGroup[]> {
-		const ret = await this.client.fetchHandler.fetch('GET', 'Groups', `/users/${this.id}/groups/roles`, {
-			params: {
-				includeLocked: includelocked,
-				includeNotificationPreferences: includeNotificationPreferences,
+				if (maxResults && returnData.length >= maxResults) {
+					break loop;
+				}
 			}
-		});
 
-		return ret.data;
+			nextPageCursor = request.nextPageCursor
+		}
 
+		return returnData;
 	}
 
+	/*
+		Methods related to the GroupsV1 API
+		Docs: https://groups.roblox.com/docs/index.html
+	*/
 
-	async fetchUserAvatarThumbnailUrl(size: AvatarSize = AvatarSize["150x150"], format: AvatarImageTypes = "Png", isCircular = false): Promise<string> {
-		const ret = await this.client.fetchHandler.fetch('GET', "Thumbnails", "/users/avatar", {
-			params: {
-				userIds: [this.id],
-				size: size,
-				format: format,
-				isCircular: isCircular,
-			}
-		});
-		return ret.data[0].imageUrl;
-	}
-	
-	async fetchUserHeadshotUrl(size: AvatarSize = AvatarSize["150x150"], format: AvatarImageTypes = "Png", isCircular = false): Promise<string> {
-		const ret = await this.client.fetchHandler.fetch('GET', "Thumbnails", "/users/avatar-headshot", {
-			params: {
-				userIds: [this.id],
-				size: size,
-				format: format,
-				isCircular: isCircular,
-			}
-		});
-		return ret.data[0].imageUrl;
-	}
-	
-	async fetchRoles(includelocked = false, includeNotificationPreferences = false): Promise<UserRoleManager> {
-		return new UserRoleManager(this.client, await this.fetchRawRoles(includelocked, includeNotificationPreferences));
+	//! To be completed
 
+	/*
+		Methods related to the InventoryV1 API
+		Docs: https://inventory.roblox.com/docs/index.html
+	*/
 
-	}
-
-
-	async inGroup(groupId: number): Promise<boolean> {
-		return (await this.fetchRawRoles()).some((group) => group.group.id === groupId);
+	async canViewInventory(): Promise<boolean> {
+		return (await this.client.fetchHandler.fetch('GET', 'Inventory', `/users/${this.id}/can-view-inventory`)).canView;
 	}
 
 	async getOwnedAsset(type: ItemTypes, id: number): Promise<OwnedItem | undefined> {
 		try {
-			return await this.client.fetchHandler.fetch('GET', 'Inventory', `/users/${this.id}/items/${type}/${id}`);
+			return (await this.client.fetchHandler.fetch('GET', 'Inventory', `/users/${this.id}/items/${type}/${id}`)).data[0];
 		} catch {
 			return undefined;
 		}
-
 	}
 
 	async ownsAsset(type: ItemTypes, assetId: number): Promise<boolean> {
@@ -112,38 +106,194 @@ class User {
 		}
 	}
 
-
 	async ownsBadge(badgeId: number): Promise<boolean> {
 		return await this.ownsAsset(ItemTypes.Badge, badgeId);
 	}
 
-	async ownsGamePass(gamePassId: number): Promise<boolean> {
-		return await this.ownsAsset(ItemTypes.GamePass, gamePassId);
+	async ownsGamepass(gamepassId: number): Promise<boolean> {
+		return await this.ownsAsset(ItemTypes.GamePass, gamepassId);
 	}
 
-
-	async getBadgeAwardedDate(badgeId: number): Promise<Date | undefined> {
-		const response: AwardedBadge | undefined = await this.client.fetchHandler.fetch('GET', 'Badges', `/users/${this.id}/badges/${badgeId}/awarded-date`)
-		if (response) {
-			return new Date(response.awardedDate);
-		}
-
+	async ownsBundle(bundleId: number): Promise<boolean> {
+		return await this.ownsAsset(ItemTypes.Bundle, bundleId)
 	}
-
-	async canViewInventory(): Promise<boolean> {
-		return (await this.client.fetchHandler.fetch('GET', 'Inventory', `/users/${this.id}/can-view-inventory`)).canView;
-	}
-
 
 	/*
-	async fetchFavoriteGames() : Promise<number[]> {
-		// WIP
-	}
+		Methods related to the AvatarV1 & V2 API
+		Docs: https://avatar.roblox.com/docs/index.html
 	*/
-	
-	toString() {
-		if (this.name === this.displayName) return this.name;
-		return `${this.displayName} (@${this.name})`;
+
+	async fetchAvatarV1(): Promise<UserAvatarV1> {
+		return (await this.client.fetchHandler.fetch("GET", "Avatar", `/users/${this.id}/avatar`))
+	}
+
+	async fetchAvatarV2(): Promise<UserAvatarV2> {
+		return (await this.client.fetchHandler.fetch("GET", "AvatarV2", `/avatar/users/${this.id}/avatar`))
+	}
+
+	/*
+		Methods related to the ThumbnailsV1 API
+		Docs: https://thumbnails.roblox.com/docs/index.html
+	*/
+
+	async fetchUserAvatarThumbnailUrl(size: AvatarSize = AvatarSize["150x150"], format: AvatarImageTypes = "Png", isCircular = false): Promise<string> {
+		return (await this.client.fetchHandler.fetch("GET", "Thumbnails", "/users/avatar", {
+			params: {
+				userIds: [this.id],
+				size: size,
+				format: format,
+				isCircular: isCircular,
+			}
+		})).data[0].imageUrl;
+	}
+
+	async fetchUserHeadshotUrl(size: AvatarSize = AvatarSize["150x150"], format: AvatarImageTypes = "Png", isCircular = false): Promise<string> {
+		return (await this.client.fetchHandler.fetch("GET", "Thumbnails", "/users/avatar-headshot", {
+			params: {
+				userIds: [this.id],
+				size: size,
+				format: format,
+				isCircular: isCircular,
+			}
+		})).data[0].imageUrl;
+	}
+
+	/*
+		Methods related to the FriendsV1 API
+		Docs: https://friends.roblox.com/docs/index.html
+	*/
+
+	async fetchFriendMetadata(): Promise<FriendMetadata> {
+		return await this.client.fetchHandler.fetch("GET", "Friends", "/metadata", {
+			params: {
+				targetUserId: this.id
+			}
+		})
+	}
+
+	//? Friends
+
+	async fetchFriends(maxResults?: number): Promise<Friend[]> {
+		if (!this.client.isLoggedIn()) throw new Error("You must be authenticated to view someone's friend list.");
+
+		const returnData = [] as Friend[];
+
+		for (const friend of (await this.client.fetchHandler.fetch("GET", "Friends", `/users/${this.id}/friends`)).data) {
+			returnData.push(await factory.createFriend(this.client, friend, this))
+			if (maxResults && returnData.length >= maxResults) break;
+		}
+
+		return returnData;
+	}
+
+
+	async fetchFriendCount(useCache = true): Promise<number> {
+		return (await this.client.fetchHandler.fetch("GET", "Friends", `/users/${this.id}/friends/count`, {
+			usecache: useCache
+		})).count
+	}
+
+	//? Followers
+
+	async fetchFollowers(sortOrder: SortOrder, maxResults?: number): Promise<User[]> {
+		const returnData = [] as User[];
+		let nextPageCursor = "NONE";
+
+		loop: while (nextPageCursor) {
+			const request = await this.client.fetchHandler.fetch("GET", "Friends", `/users/${this.id}/followers`, {
+				params: {
+					limit: 100,
+					cursor: nextPageCursor !== "NONE" && nextPageCursor || undefined,
+					sortOrder: sortOrder
+				}
+			})
+
+			for (const user of request.data) {
+				returnData.push(await factory.createUser(this.client, user));
+
+				if (maxResults &&  returnData.length >= maxResults) {
+					break loop;
+				}
+			}
+
+			nextPageCursor = request.nextPageCursor
+		}
+
+		return returnData;
+	}
+
+
+	async fetchFollowerCount(useCache = true): Promise<number> {
+		return (await this.client.fetchHandler.fetch("GET", "Friends", `/users/${this.id}/followers/count`, {
+			usecache: useCache
+		})).count
+	}
+
+	//? Followings
+
+	async fetchFollowings(sortOrder: SortOrder, maxResults?: number): Promise<User[]> {
+		const returnData = [] as User[];
+		let nextPageCursor = "NONE";
+
+		loop: while (nextPageCursor) {
+			const request = await this.client.fetchHandler.fetch("GET", "Friends", `/users/${this.id}/followings`, {
+				params: {
+					limit: 100,
+					cursor: nextPageCursor !== "NONE" && nextPageCursor || undefined,
+					sortOrder: sortOrder
+				}
+			})
+
+			for (const user of request.data) {
+				returnData.push(await factory.createUser(this.client, user));
+
+				if (maxResults && returnData.length >= maxResults) {
+					break loop;
+				}
+			}
+
+			nextPageCursor = request.nextPageCursor
+		}
+
+		return returnData;
+	}
+
+	async fetchFollowingsCount(useCache = true): Promise<number> {
+		return (await this.client.fetchHandler.fetch("GET", "Friends", `/users/${this.id}/followings/count`, {
+			usecache: useCache
+		})).count
+	}
+
+	/*
+		Methods related to the AccountSettingsV1 API
+		Docs: https://accountsettings.roblox.com/docs/index.html
+	*/
+
+	async block(): Promise<void> {
+		if (!this.client.isLoggedIn()) throw new Error("You must be authenticated to block users.");
+
+		await this.client.fetchHandler.fetch("POST", "AccountSettings", `/users/${this.id}/block`)
+	}
+
+	async unblock(): Promise<void> {
+		if (!this.client.isLoggedIn()) throw new Error("You must be authenticated to unblock users.");
+
+		await this.client.fetchHandler.fetch("POST", "AccountSettings", `/users/${this.id}/unblock`)
+	}
+
+	/*
+		Methods related to the PremiumFeaturesV1 API
+		Docs: https://premiumfeatures.roblox.com/docs/index.html
+	*/
+
+	async hasPremium(useCache = true): Promise<boolean> {
+		return await this.client.fetchHandler.fetch("GET", "PremiumFeatures", `/users/${this.id}/validate-membership`, { usecache: useCache })
+	}
+
+	// Miscellaneous
+
+	toString(): string {
+		return `${this.name}:${this.id}`
 	}
 }
 
--- tests/user.test.ts
+++ tests/user.test.ts
@@ -1,51 +1,252 @@
-import WrapBlox from "../src/index.js";
+import Friend from "../src/Classes/Friend.js";
+import FetchError from "../src/Classes/Internal/FetchError.js";
+import WrapBlox, { ItemTypes, User } from "../src/index.js";
+import dotenv from "dotenv";
+dotenv.config();
 
 const client = new WrapBlox();
+const userId = 2897964600;
 
+test("login", async () => {
+	if (!process.env.TESTCOOKIE) {
+		console.log("No cookie provided, skipping...")
+		return;
+	}
 
-test("getThumbnail", async () => {
-	const user = await client.fetchUser(1);
-	const thumbnail = await user.fetchUserAvatarThumbnailUrl();
-	
-	
-	expect(thumbnail).toBeDefined();
+	const user = await client.login(process.env.TESTCOOKIE)
+	console.log(`Logged in as ${user.name}:${user.id}`)
+
+	expect(user).toBeDefined();
 })
 
-test("groupTest", async () => {
-	const user = await client.fetchUser(1494607434);
-	expect(await user.inGroup(645836)).toBeTruthy();
-});
+test("fetchUser", async () => {
+	const user = await client.fetchUser(userId);
 
-test("role", async () => {
-	const user = await client.fetchUser(1494607434);
-	const roles = await user.fetchRoles()
-	const role = await roles.getRole(645836)
-	
-	
-	expect(role).toBeDefined();
-});
+	console.log(`Fetched user:\n${user}`)
+	expect(user).toBeDefined();
+})
 
-test("userLookup", async () => {
-	const users = await client.searchUsers("Cater")
+test("fetchUsernameHistory", async () => {
+	const user = await client.fetchUser(userId);
+	const usernameHistory = await user.fetchUsernameHistory();
+
+	console.log(`Fetched username history:\n${usernameHistory}`)
+
+	expect(usernameHistory).toBeDefined();
+})
+
+//! Groups to be completed
+
+test("canViewInventory", async () => {
+	const user = await client.fetchUser(userId);
+	const canView = await user.canViewInventory();
+
+	console.log(`Can view inventory: ${canView}`);
+
+	expect(canView).toBeDefined();
+})
+
+test("ownsAsset", async () => {
+	const user = await client.fetchUser(userId);
+	const bool = await user.ownsAsset(ItemTypes.GamePass, 776368);
+
+	console.log(`Owns asset: ${bool}`);
+
+	expect(bool).toBeDefined();
+})
+
+test("ownsBadge", async () => {
+	const user = await client.fetchUser(userId);
+	const bool = await user.ownsBadge(150538398);
+
+	console.log(`Owns Badge: ${bool}`);
+
+	expect(bool).toBeDefined();
+})
+
+test("ownsGamepass", async () => {
+	const user = await client.fetchUser(userId);
+	const bool = await user.ownsGamepass(776368);
+
+	console.log(`Owns Gamepass: ${bool}`);
+
+	expect(bool).toBeDefined();
+})
+
+test("ownsBundle", async () => {
+	const user = await client.fetchUser(userId);
+	const bool = await user.ownsBundle(201);
+
+	console.log(`Owns Bundle: ${bool}`);
+
+	expect(bool).toBeDefined();
+})
+
+test("getOwnedAsset", async () => {
+	const user = await client.fetchUser(userId);
+	const asset = await user.getOwnedAsset(ItemTypes.GamePass, 776368)
+
+	console.log("Owned asset:\n",asset)
+
+	expect(asset).toBeDefined();
+})
+
+test("fetchAvatarV1", async () => {
+	const user = await client.fetchUser(userId);
+	const avatar = await user.fetchAvatarV1();
+
+	console.log("User AvatarV1:\n",avatar)
+
+	expect(avatar).toBeDefined();
+})
+
+test("fetchAvatarV2", async () => {
+	const user = await client.fetchUser(userId);
+	const avatar = await user.fetchAvatarV2();
+
+	console.log("User AvatarV2:\n",avatar)
+
+	expect(avatar).toBeDefined();
+})
+
+
+test("fetchUserAvatarThumbnailUrl", async () => {
+	const user = await client.fetchUser(userId);
+	const imageUrl = await user.fetchUserAvatarThumbnailUrl();
+
+	console.log(`Fetched imageUrl for Avatar Thumbnail:\n${imageUrl}`);
+
+	expect(imageUrl).toBeDefined();
+})
+
+test("fetchUserHeadshotUrl", async () => {
+	const user = await client.fetchUser(userId);
+	const imageUrl = await user.fetchUserHeadshotUrl();
+
+	console.log(`Fetched imageUrl for Avatar Headshot:\n${imageUrl}`);
+
+	expect(imageUrl).toBeDefined();
+})
+
+test("fetchUserFriendMetadata", async () => {
+	const user = await client.fetchUser(userId);
+	const metadata = await user.fetchFriendMetadata();
+
+	console.log("Fetched metadata:\n",metadata);
+
+	expect(metadata).toBeDefined();
+})
+
+
+test("fetchUserFriends", async () => {
+	if (!client.isLoggedIn()) {
+		console.log("Not logged in, skipping...")
+		return;
+	}
+
+	const user = await client.fetchUser(userId);
+	const users = await user.fetchFriends();
+
+	console.log(`Fetched friends [${users.length}]\n`,users.map((data: Friend) => data.toString()).join("\n"))
+
 	expect(users).toBeDefined();
 })
 
-test("badgeTest", async () => {
-	const user = await client.fetchUser(1494607434);
-	const ownsBadge = await user.ownsBadge(150538265);
-	const ownsBadge2 = await user.ownsBadge(2124453108);
-	
-	const date = await user.getBadgeAwardedDate(150538265);
-	
-	expect(date).toBeDefined();
-	expect(ownsBadge).toBeTruthy();
-	expect(ownsBadge2).toBeFalsy();
-	
+
+test("fetchUserFriendCount", async () => {
+	const user = await client.fetchUser(userId);
+	const count = await user.fetchFriendCount();
+
+	console.log(`Fetched friend count: [${count}]`);
+
+	expect(count).toBeDefined();
 })
 
-test("getUser", async () => {
-	const user = await client.fetchUser(1);
-	console.log(user)
-	console.log(`Fetched ${user}`)
-	expect(user).toBeDefined();
+
+test("fetchUserFollowers", async () => {
+	const user = await client.fetchUser(userId);
+	const users = await user.fetchFollowers("Asc", 10)
+
+	console.log(`Fetched followers [${users.length}]\n`,users.map((data: User) => data.toString()).join("\n"))
+
+	expect(users).toBeDefined();
+})
+
+
+test("fetchUserFollowerCount", async () => {
+	const user = await client.fetchUser(userId);
+	const count = await user.fetchFollowerCount();
+	
+	console.log(`Fetched follower count: [${count}]`);
+
+	expect(count).toBeDefined();
+})
+
+
+test("fetchUserFollowings", async () => {
+	const user = await client.fetchUser(userId);
+	const users = await user.fetchFollowings("Asc", 10)
+
+	console.log(`Fetched followings [${users.length}]\n`,users.map((data: User) => data.toString()).join("\n"))
+
+	expect(users).toBeDefined();
+})
+
+
+test("fetchUserFollowingsCount", async () => {
+	const user = await client.fetchUser(userId);
+	const count = await user.fetchFollowingsCount();
+	
+	console.log(`Fetched following count: [${count}]`);
+	console.log(user.accountAge)
+
+	expect(count).toBeDefined();
+})
+
+test("block", async () => {
+	if (!client.isLoggedIn()) {
+		console.log("Not logged in, skipping...")
+		return;
+	}
+
+	const user = await client.fetchUser(userId);
+	try {
+		await user.block();
+	} catch (error) {
+		if (!(error instanceof FetchError)) return;
+		const body = await error.response.json();
+
+		console.log(`Failed to block [${error.message}]:\n${body.errors[0]?.code}: ${body.errors[0]?.message}`)
+	}
+})
+
+test("unblock", async () => {
+	if (!client.isLoggedIn()) {
+		console.log("Not logged in, skipping...")
+		return;
+	}
+
+	const user = await client.fetchUser(userId);
+	try {
+		await user.unblock();
+	} catch (error) {
+		if (!(error instanceof FetchError)) return;
+		const body = await error.response.json();
+
+		console.log(`Failed to unblock [${error.message}]:\n${body.errors[0]?.code}: ${body.errors[0]?.message}`)
+	}
+})
+
+test("hasPremium", async () => {
+	if (!client.isLoggedIn()) {
+		console.log("Not logged in, skipping...")
+		return;
+	}
+
+	const user = await client.fetchUser(userId);
+	const hasPremium = await user.hasPremium();
+
+	console.log(`hasPremium: ${hasPremium}`);
+
+	expect(hasPremium).toBeDefined();
 })
