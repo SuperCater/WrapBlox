e7e7e8d656ec7c1f768ad6c29ba7af105aab29c4
Purple_Creativity <thepurplecreativity@gmail.com>
2024-11-05 18:26:38 +0100

    getErrors()

--- src/Classes/Internal/FetchError.ts
+++ src/Classes/Internal/FetchError.ts
@@ -1,3 +1,8 @@
+export type FetchedError = {
+	code: number,
+	message: string
+};
+
 export default class FetchError extends Error {
 	response : Response;
 
@@ -6,9 +11,14 @@ export default class FetchError extends Error {
 		this.response = response;
 	};
 
-	async format(): Promise<string> {
+	async getErrors(): Promise<FetchedError[]> {
 		const body = await this.response.json();
-		const formattedErrors = body.errors.map((error: {code: number, message: string}) => `[${error.code}]: ${error.message}`).join("\n");
+		return body.errors;
+	};
+
+	async format(): Promise<string> {
+		const errors = await this.getErrors();
+		const formattedErrors = errors.map((error: {code: number, message: string}) => `[${error.code}]: ${error.message}`).join("\n");
 
 		return `[${this.response.status} ${this.response.statusText}]:\n${formattedErrors}`;
 	};
